313a4440c7464a2d9f2af8db8f6d9763f444dd2c
Ethan Benjamin <ethan.a.benjamin@gmail.com>
2012-04-23 21:28:43 -0400

    minimize DOM access

--- Renderer.js
+++ Renderer.js
@@ -94,24 +94,28 @@ Renderer.prototype.drawTriangle = function(p1, p2, p3){ //,useTexture) {
 	var maxY = Math.floor(Math.max(p1.y, p2.y, p3.y,0));
 	var width = maxX - minX;
 	var height = maxY - minY;
+	
+	var canvWidth = this.canvas.width;
+	var canvHeight = this.canvas.height;
+	var pixelData = this.imageData.data;
 
 	for(var x = minX; x < maxX; x++) {
 		for(var y = minY; y < maxY; y++) {
 			var b1 = ((p2.y - p3.y) * (x - p3.x) + (p3.x - p2.x) * (y - p3.y)) / ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
 			var b2 = ((p3.y - p1.y) * (x - p3.x) + (p1.x - p3.x) * (y - p3.y)) / ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
 			var b3 = 1 - b1 - b2;
-			var index = y * this.imageData.width * 4 + x * 4;
-			var depthIndex = y * this.imageData.width + x;
+			var index = y * canvHeight * 4 + x * 4;
+			var depthIndex = y * canvWidth + x;
 			var depth = Math.floor(b1 * p1.z + b2 * p2.z + b3 * p3.z);
 			if((b1 >= 0 && b1 <= 1) && (b2 >= 0 && b2 <= 1) && (b3 >= 0 && b3 <= 1)) {
 
 				if(depth > this.depthMap[depthIndex]) {
-					this.imageData.data[index] = b1 * p1.r + b2 * p2.r + b3 * p3.r;
-					this.imageData.data[index + 1] = b1 * p1.g + b2 * p2.g + b3 * p3.g;
-					this.imageData.data[index + 2] = b1 * p1.b + b2 * p2.b + b3 * p3.b;
-					this.imageData.data[index + 3] = 255;
+					pixelData[index] = b1 * p1.r + b2 * p2.r + b3 * p3.r;
+					pixelData[index + 1] = b1 * p1.g + b2 * p2.g + b3 * p3.g;
+					pixelData[index + 2] = b1 * p1.b + b2 * p2.b + b3 * p3.b;
+					pixelData[index + 3] = 255;
 					if(b1 < .02 || b2 < .02 || b3 < .02) {
-						this.imageData.data[index] = this.imageData.data[index + 1] = this.imageData.data[index + 2] = 0;
+						pixelData[index] = pixelData[index + 1] = pixelData[index + 2] = 0;
 					}
 					/*if(useTexture ){
 						var tx = Math.floor(b1*p1.tx + b2*p2.tx + b3*p3.tx);
