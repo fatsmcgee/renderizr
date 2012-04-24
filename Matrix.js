573f672d2c6c21b71b0d0c09965d8aae2313fb6b
Ethan <ethan.a.benjamin@gmail.com>
2012-04-23 20:24:16 -0400

    first commit

---
+++ Matrix.js
@@ -0,0 +1,116 @@
+function Mat4(){
+	this.values = new Array();
+	for(var i = 0; i<16; i++){
+		this.values[i] = 0;
+	}
+}
+
+Mat4.prototype.loadIdentity = function(){
+	for(var i = 0; i<16; i++){
+		if(i%5==0){
+			this.values[i] = 1;
+		}
+		else{
+			this.values[i] = 0;
+		}
+	}
+	
+
+}
+
+Mat4.prototype.rowAt= function(i){
+	return [this.values[i*4],this.values[i*4+1],this.values[i*4+2],this.values[i*4+3]];
+}
+Mat4.prototype.colAt = function(i){
+	return [this.values[i],this.values[i+4],this.values[i+8],this.values[i+12]];
+}
+
+Mat4.prototype.multByMat = function(otherMat4){
+	var values = [];
+	for(var i = 0; i<4; i++){
+		for(var j = 0; j<4; j++){
+			values[i*4+j] = vec4Dot(this.rowAt(i),otherMat4.colAt(j));
+		}
+	}
+	this.values = values;
+}
+
+Mat4.prototype.matMultBy= function(otherMat4){
+	var values = [];
+	for(var i = 0; i<4; i++){
+		for(var j = 0; j<4; j++){
+			values[i*4+j] = vec4Dot(otherMat4.rowAt(i),this.colAt(j));
+		}
+	}
+	this.values = values;
+}
+
+
+
+Mat4.prototype.multVec = function(vec4){
+	if(vec4.x !== undefined){
+		vec4 = [vec4.x,vec4.y,vec4.z,vec4.w === undefined? 1:vec4.w];
+	}
+	return {x:vec4Dot(this.rowAt(0),vec4)
+			,y:vec4Dot(this.rowAt(1),vec4)
+			,z:vec4Dot(this.rowAt(2),vec4)
+			,w:vec4Dot(this.rowAt(3),vec4)};
+}
+
+Mat4.prototype.loadFrustum = function(near,far,left,right,bottom,top){
+	if(left === undefined){
+		left = bottom = -1;
+		right = top = 1;
+	}
+	var A = (right+left)/(right-left);
+	var B = (top+bottom)/(top-bottom);
+	var C = -((far+near)/(far-near));
+	var D = -((2*far*near)/(far-near));
+	
+	this.values = [(2*near)/(right-left),0,A,0,
+				   0,(2*near)/(top-bottom),B,0,
+				   0,0,C,D,
+				   0,0,-1,0];
+}
+
+Mat4.prototype.loadTransMat = function(x,y,z){
+	this.values=[1,0,0,x,
+				 0,1,0,y,
+				 0,0,1,z,
+				 0,0,0,1];
+				 
+}
+
+Mat4.prototype.loadRotationMat = function(theta,x,y,z){
+	var size = x*x + y*y + z*z;
+	if(size >1){
+		x/=size;
+		y/=size;
+		z/=size;
+	}
+	var c = Math.cos(theta);
+	var s = Math.sin(theta);
+	this.values = [ x*x*(1-c)+c, x*y*(1-c)-z*s, x*z*(1-c)+y*s,0,
+					y*x*(1-c)+z*s, y*y*(1-c)+c, y*z*(1-c)-x*s,0,
+					x*z*(1-c)-y*s, y*z*(1-c)+x*s, z*z*(1-c)+c, 0,
+					0, 0, 0, 1];
+};
+
+Mat4.prototype.rotate = function(theta,x,y,z){
+	var otherMat = new Mat4();
+	otherMat.loadRotationMat(theta,x,y,z);
+	this.matMultBy(otherMat);
+}
+
+Mat4.prototype.translate = function(x,y,z){
+	var otherMat = new Mat4();
+	otherMat.loadTransMat(x,y,z);
+	this.matMultBy(otherMat);
+}
+
+var vec4Dot = function(vec1,vec2){
+	return vec1[0]*vec2[0] + vec1[1]*vec2[1]+vec1[2]*vec2[2]+vec1[3]*vec2[3];
+}
+
+
+
---
+++ Renderer.js
@@ -0,0 +1,130 @@
+function Renderer(canvas){
+	this.canvas = canvas;
+	this.ctx = canvas.getContext('2d');			
+	this.imageData = this.ctx.createImageData(canvas.width, canvas.height);
+	this.depthMap = new Uint32Array(canvas.width * canvas.height);
+	this.maxDepthMap = new Uint32Array(canvas.width * canvas.height);
+	for(var i = 0; i < this.maxDepthMap.length; i++) {
+		this.maxDepthMap[i] = 0;
+	}
+	this.viewerZ = 0;
+	this.textures = [];
+	this.modelViewMat = new Mat4();
+	this.modelViewMat.loadIdentity();
+	this.projectionMat = new Mat4();
+	this.projectionMat.loadFrustum(1,2);
+}
+
+Renderer.prototype.addTexture = function(img){
+	
+	/*var canv2 = document.createElement("canvas");
+	canv2.width = img.width;
+	canv2.height = img.height;
+	var ctx2 = canv2.getContext('2d');
+	ctx2.drawImage(img,0,0);*
+	var imData = ctx2.getImageData(0,0,canv2.width,canv2.height);*/
+	imData = this.ctx.createImageData(256,256);
+	for(var i = 0; i<imData.data.length; i+=4){
+		var row = i/(256*4);
+		var col = i%(256*4);
+		if((Math.floor(row/32)%2)^ (Math.floor(col/32)%2) == 1){
+			imData.data[i] = imData.data[i+1] = imData.data[i+2] = 0;
+			imData.data[i+3] = 255;
+		}
+	}
+	this.textures.push(imData);
+	return this.textures.length -1;
+}
+
+Renderer.prototype.worldToScreen = function(pt){
+	//transform from model view coordinates to eye coordinates
+	var transPoint = this.modelViewMat.multVec(pt);
+	//transform from camera(eye) to clip space with projection matrix
+	var transPoint = this.projectionMat.multVec(transPoint);
+	//transform from clip space to NDC by dividing by w
+	//NDC has -1 to 1 coordinates for x,y,and z
+	transPoint.x/=transPoint.w;
+	transPoint.y/=transPoint.w;
+	transPoint.z/=transPoint.w;
+	//transform from NDC to window coordinates
+	transPoint.x = transPoint.x*this.canvas.width + this.canvas.width/2;
+	transPoint.y = this.canvas.height - (transPoint.y*this.canvas.height + this.canvas.height/2);
+	//take z from [-1,1] space to [0,2000] for greater depth fidelity on an integer basis
+	transPoint.z = (transPoint.z+1) * 2000; 
+	
+	//copy the parameters provided by the user
+	transPoint.r = pt.r;
+	transPoint.g = pt.g;
+	transPoint.b = pt.b;
+	transPoint.tx = pt.tx;
+	transPoint.ty = pt.ty;
+	
+	return transPoint;
+	
+}
+		
+Renderer.prototype.queueTriangle = function(p1, p2, p3) {
+	this.triangles.push([this.worldToScreen(p1), this.worldToScreen(p2), this.worldToScreen(p3)]);
+}
+
+Renderer.prototype.clearImageData = function() {
+	for(var i = 0; i < this.imageData.data.length; i++) {
+		this.imageData.data[i] = 0;
+	}
+}
+
+Renderer.prototype.beginRender = function() {
+	this.clearImageData();
+	this.depthMap.set(this.maxDepthMap, 0);
+	this.triangles = [];
+}
+
+Renderer.prototype.endRender = function() {
+	for(var i in this.triangles) {
+		var tris = this.triangles[i];
+		this.drawTriangle(tris[0], tris[1], tris[2],i%2==0);
+	}
+	this.ctx.putImageData(this.imageData, 0, 0);
+}
+
+Renderer.prototype.drawTriangle = function(p1, p2, p3){ //,useTexture) {
+	var minX = Math.floor(Math.min(p1.x, p2.x, p3.x,this.canvas.width-1));
+	var maxX = Math.floor(Math.max(p1.x, p2.x, p3.x,0));
+	var minY = Math.floor(Math.min(p1.y, p2.y, p3.y,this.canvas.height-1));
+	var maxY = Math.floor(Math.max(p1.y, p2.y, p3.y,0));
+	var width = maxX - minX;
+	var height = maxY - minY;
+
+	for(var x = minX; x < maxX; x++) {
+		for(var y = minY; y < maxY; y++) {
+			var b1 = ((p2.y - p3.y) * (x - p3.x) + (p3.x - p2.x) * (y - p3.y)) / ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
+			var b2 = ((p3.y - p1.y) * (x - p3.x) + (p1.x - p3.x) * (y - p3.y)) / ((p2.y - p3.y) * (p1.x - p3.x) + (p3.x - p2.x) * (p1.y - p3.y));
+			var b3 = 1 - b1 - b2;
+			var index = y * this.imageData.width * 4 + x * 4;
+			var depthIndex = y * this.imageData.width + x;
+			var depth = Math.floor(b1 * p1.z + b2 * p2.z + b3 * p3.z);
+			if((b1 >= 0 && b1 <= 1) && (b2 >= 0 && b2 <= 1) && (b3 >= 0 && b3 <= 1)) {
+
+				if(depth > this.depthMap[depthIndex]) {
+					this.imageData.data[index] = b1 * p1.r + b2 * p2.r + b3 * p3.r;
+					this.imageData.data[index + 1] = b1 * p1.g + b2 * p2.g + b3 * p3.g;
+					this.imageData.data[index + 2] = b1 * p1.b + b2 * p2.b + b3 * p3.b;
+					this.imageData.data[index + 3] = 255;
+					if(b1 < .02 || b2 < .02 || b3 < .02) {
+						this.imageData.data[index] = this.imageData.data[index + 1] = this.imageData.data[index + 2] = 0;
+					}
+					/*if(useTexture ){
+						var tx = Math.floor(b1*p1.tx + b2*p2.tx + b3*p3.tx);
+						var ty = Math.floor(b1*p1.ty + b2*p2.ty + b3*p3.ty);
+						var texIndex = Math.floor(ty*256*4 + tx*4);
+						this.imageData.data[index] = this.textures[0].data[texIndex];
+						this.imageData.data[index+1] = this.textures[0].data[texIndex+1];
+						this.imageData.data[index+2] = this.textures[0].data[texIndex+2];
+						this.imageData.data[index+3] = this.textures[0].data[texIndex+3];
+					}*/
+					this.depthMap[depthIndex] = depth;
+				}
+			}
+		}
+	}
+}
