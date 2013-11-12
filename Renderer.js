41696a64f11b2401194ff4235aaf9810f02363e6
unknown <EBenjami@EBENJAMI-BOS-D.gmo.tld>
2013-11-12 15:01:21 -0500

    First pages commit

---
+++ Renderer.js
@@ -0,0 +1,237 @@
+function Renderer(canvas){
+	this.canvas = canvas;
+	this.ctx = canvas.getContext('2d');			
+	this.imageData = this.ctx.createImageData(canvas.width, canvas.height);
+	this.blankImageData = this.ctx.createImageData(canvas.width,canvas.height);
+	this.depthMap = new Uint32Array(canvas.width * canvas.height);
+	this.maxDepthMap = new Uint32Array(canvas.width * canvas.height);
+	
+	var maxInt = Math.pow(2,32)-1;
+	for(var i = 0; i < this.maxDepthMap.length; i++) {
+		this.maxDepthMap[i] = maxInt;
+	}
+	//blank image data considered to be fully opaque white
+	for(var i = 0; i<this.blankImageData.data.length; i++){
+		this.blankImageData.data[i] = this.blankImageData.data[i+1] = this.blankImageData.data[i+2] = 0;
+		this.blankImageData.data[i+3] = 255;
+	}
+	
+	this.matrixStack = [];
+	
+	this.restoreDefaults();
+}
+
+//set reasonable defaults for the renderer to use with its shaders and matrices
+Renderer.prototype.restoreDefaults = function(){
+	this.modelViewMat = new Mat4();
+	this.modelViewMat.loadIdentity();
+	this.projectionMat = new Mat4();
+	this.projectionMat.loadFrustum(1,2);
+	
+	this.setFragmentShader(Renderer.defaultFragmentShader);
+	this.setVertexShader(Renderer.defaultVertexShader);
+}
+
+Renderer.prototype.pushMatrices = function(){
+	var modeViewCopy = this.modelViewMat.copy();
+	var projectionCopy = this.projectionMat.copy();
+	this.matrixStack.push({modelView:modelViewCopy,projection:projectionCopy});
+}
+
+Renderer.prototype.popMatrices = function(){
+	
+}
+
+Renderer.defaultVertexShader = function(R,pt,uniform){
+	//transform from model view coordinates to eye coordinates
+	R.modelViewMat.multVector(pt);
+	//transform from camera(eye) to clip space with projection matrix
+	R.projectionMat.multVector(pt);
+	
+}
+
+Renderer.defaultFragmentShader = function(R,pt,uniform){
+	pt.r = pt.r;
+	pt.g = pt.g;
+	pt.b = pt.b;
+}
+
+Renderer.prototype.setVertexShader = function(shaderFunc){
+	this.vertexShader = shaderFunc;
+}
+
+Renderer.prototype.setFragmentShader = function(shaderFunc){
+	this.fragmentShader = shaderFunc;
+}
+
+Renderer.prototype.setDefaultFragmentShader = function(){
+	this.fragmentShader = Renderer.defaultFragmentShader;
+}
+
+Renderer.prototype.setDefaultVertexShader = function(){
+	this.vertexShader = Renderer.defaultVertexShader;
+}
+
+Renderer.prototype.clearImageData = (function(){
+	if( navigator.userAgent.toLowerCase().indexOf('chrome') > -1){
+		return function(){
+			var data = this.imageData.data;
+			var i = data.length-1;
+			do{
+				data[i] = 0;
+			}
+			while(i--);
+		};
+	}
+	else{
+		return function(){
+			this.imageData.data.set(this.blankImageData.data,0,0);
+		};
+	}
+})();
+
+Renderer.prototype.clearColor = function(r,g,b){
+	var data = this.imageData.data;
+	//This insane micro-optimization doubles the frame rate in chrome!!
+	var i = data.length;
+	do{
+		data[i-4] = r;
+		data[i-3] = g;
+		data[i-2] = b;
+		data[i-1] = 255;
+	}
+	while(i-=4);
+	this.clearDepthMap();
+}
+
+Renderer.prototype.clear = function(){
+	this.clearImageData();
+	//this.imageData.data.set(this.blankImageData.data,0,0);
+	this.clearDepthMap();
+}
+
+Renderer.prototype.clearDepthMap = function(){
+	this.depthMap.set(this.maxDepthMap,0);
+}
+
+
+Renderer.prototype.update = function(){
+	this.ctx.putImageData(this.imageData,0,0);
+}
+
+RenderModes = {Triangles:0,TriangleFan:1,Quads:2};
+
+Renderer.prototype.drawElements = function(vertices,mode){
+	if(mode === undefined){
+		mode = RenderModes.Triangles;
+	}
+	//allow user to pass objects that are equal to each other
+	vertices = vertices.map(Utility.ShallowCopy);
+	//first transform each element with the vertex shader and normalize it's w value'
+	for(var i = 0; i<vertices.length; i++){
+		var vert = vertices[i];
+		this.vertexShader(this,vert,this.uniform);
+		//vertex shader is responsible for model view and projection transformations
+		//see defaultVertexShader for normal implementation
+		//transform from clip space to NDC by dividing by w
+		//NDC has -1 to 1 coordinates for x,y,and z
+		//yes, -.5 z is visible! weird but openGL does it
+		vert.x/=vert.w;
+		vert.y/=vert.w;
+		vert.z/=vert.w;
+		
+		//make depth a value that can be easily indexed in an array
+		vert.depth = (vert.z+1)*2000;
+		
+		//finally convert from NDC coordinates to window coordinates
+		vert.screenX = vert.x*this.canvas.width/2 + this.canvas.width/2;
+		vert.screenY = this.canvas.height - (vert.y*this.canvas.height/2 + this.canvas.height/2);
+	}
+	//now rasterize the primitives based on the raster mode
+	if(mode == RenderModes.Triangles){
+		for(var i = 0; i<vertices.length; i+=3){
+			this.drawTriangle(vertices[i],vertices[i+1],vertices[i+2]);
+		}
+	}
+	else if (mode == RenderModes.Quads){
+		//treat vertices a,b,c,d as two triangles abc and cad
+		for(var i =0; i<vertices.length; i+=4){
+			this.drawTriangle(vertices[i],vertices[i+1],vertices[i+2]);
+			this.drawTriangle(vertices[i+2],vertices[i],vertices[i+3]);
+		}
+	}
+}
+
+Renderer.prototype.setUniform = function(uniformObj){
+	this.uniform = uniformObj;
+}
+
+Renderer.prototype.drawTriangle = function(p1, p2, p3){ 
+	var minX = Math.floor(Math.max(0,Math.min(p1.screenX, p2.screenX, p3.screenX,this.canvas.width-1)));
+	var maxX = Math.floor(Math.min(this.canvas.width-1,Math.max(p1.screenX, p2.screenX, p3.screenX,0)));
+	var minY = Math.floor(Math.max(0,Math.min(p1.screenY, p2.screenY, p3.screenY,this.canvas.height-1)));
+	var maxY = Math.floor(Math.min(this.canvas.height-1,Math.max(p1.screenY, p2.screenY, p3.screenY,0)));
+	var width = maxX - minX;
+	var height = maxY - minY;
+	
+	var canvWidth = this.canvas.width;
+	var canvHeight = this.canvas.height;
+	var pixelData = this.imageData.data;
+	//includes all interpolated data that will be passed to the fragment shader
+	var interpolatedPoint = {};
+	var fragmentShader = this.fragmentShader;
+	var depthMap = this.depthMap;
+	
+	var uniform = this.uniform;
+	for(var idx in p1){
+		interpolatedPoint[idx] = p1[idx];
+	}
+	//reduce changes in barycentric coordinates as x/y increments to constants
+	var b1Denominator = ((p2.screenY - p3.screenY) * (p1.screenX - p3.screenX) + (p3.screenX - p2.screenX) * (p1.screenY - p3.screenY));
+	var b2Denominator = ((p2.screenY - p3.screenY) * (p1.screenX - p3.screenX) + (p3.screenX - p2.screenX) * (p1.screenY - p3.screenY));
+	var b1 = ((p2.screenY - p3.screenY) * (maxX- p3.screenX) + (p3.screenX - p2.screenX) * (maxY - p3.screenY)) /b1Denominator;
+	var b1ChangeOnYDec = (p2.screenX- p3.screenX )/b1Denominator;
+	var b1ChangeOnXDec = (p3.screenY - p2.screenY)/b1Denominator;
+	var b2 = ((p3.screenY - p1.screenY) * (maxX - p3.screenX) + (p1.screenX - p3.screenX) * (maxY - p3.screenY))/b2Denominator;
+	var b2ChangeOnYDec = (p3.screenX-p1.screenX)/b2Denominator;
+	var b2ChangeOnXDec = (p1.screenY-p3.screenY)/b2Denominator;	
+	
+	var xSpan = maxX-minX+1;
+	var ySpan = maxY-minY;
+	
+	//for(var x = maxX; x >minX; x--) {
+	for(var i = xSpan; i--;){
+		var x = i+minX;
+		//for(var y = minY; y < maxY; y++) {
+		for(var j = ySpan; j--;){
+			var y = j+minY;
+			var b3 = 1 - b1 - b2;
+			if(b1 >= 0 && b2 >= 0 && b3 >= 0) {
+				var index = y * canvHeight * 4 + x * 4;
+				var depthIndex = y * canvWidth + x;
+				var depth = Math.floor(b1 * p1.depth + b2 * p2.depth + b3 * p3.depth);
+				if(depth < depthMap[depthIndex]) {
+					for(var idx in interpolatedPoint){
+						interpolatedPoint[idx] = b1*p1[idx] + b2*p2[idx] + b3*p3[idx];
+					}
+					fragmentShader(this,interpolatedPoint,uniform);
+					pixelData[index] = interpolatedPoint.r;
+					pixelData[index + 1] = interpolatedPoint.g;
+					pixelData[index + 2] = interpolatedPoint.b;
+					pixelData[index + 3] = 255;
+					if(b1 < .02 || b2 < .02 || b3 < .02) {
+						pixelData[index] = pixelData[index + 1] = pixelData[index + 2] = 0;
+					}
+					
+					depthMap[depthIndex] = depth;
+				}
+			}
+			b1 += b1ChangeOnYDec;
+			b2 += b2ChangeOnYDec;
+		}
+		b1 -= (b1ChangeOnYDec * (maxY-minY));
+		b2 -= (b2ChangeOnYDec * (maxY-minY));
+		b1 += b1ChangeOnXDec;
+		b2 += b2ChangeOnXDec;
+	}
+}
---
+++ Scenes.js
@@ -0,0 +1,204 @@
+var Scenes = {};
+
+Scenes.spinningPyramid = {
+	title:"Spinning pyramid",
+	description:"Press left/right, up/down or 9/0 keys to rotate on the y,x, and z axes respectively",
+	drawUpdate: 
+	"(function() {\n\
+	var vertShader = function(R,pt,uniform){\n\
+		//transform from model view coordinates to eye coordinates\n\
+		R.modelViewMat.multVector(pt);\n\
+		//transform from camera(eye) to clip space with projection matrix)\n\
+		R.projectionMat.multVector(pt);\n\
+	}\n\
+	var fragShader = function(R,pt,uniform){\n\
+		pt.r = pt.r;\n\
+		pt.g = pt.g;\n\
+		pt.b = pt.b;\n\
+	}\n\
+	R.setVertexShader(vertShader);\n\
+	R.setFragmentShader(fragShader);\n\
+	var increment = {\n\
+		x : 0,\n\
+		y : 0,\n\
+		z : 0\n\
+	};\n\
+	var timer = {\n\
+		x : 0,\n\
+		y : 0,\n\
+		z : 0\n\
+	};\n\
+	var draw = function() {\n\
+		increment.y += Number(KeysDown[KeyCodes.LEFT])*.01;\n\
+		increment.y -= Number(KeysDown[KeyCodes.RIGHT])*.01;\n\
+		increment.x += Number(KeysDown[KeyCodes.UP]) *.01;\n\
+		increment.x -= Number(KeysDown[KeyCodes.DOWN]) *.01;\n\
+		increment.z += Number(KeysDown[KeyCodes[9]]) * .01;\n\
+		increment.z -= Number(KeysDown[KeyCodes[0]]) * .01;\n\
+\n\
+		R.clear();\n\
+		R.modelViewMat.loadIdentity();\n\
+		R.modelViewMat.rotate(timer.x,1,0,0);\n\
+		R.modelViewMat.rotate(timer.y,0,1,0);\n\
+		R.modelViewMat.rotate(timer.z,0,0,1);\n\
+		\n\
+		R.modelViewMat.translate(0,0,-1.40);\n\
+\n\
+\n\
+		var theta1 = 0;\n\
+		var theta2 =  (2 * Math.PI) / 3;\n\
+		var theta3 = (4 * Math.PI) / 3;\n\
+		var p1 = {\n\
+			x : .25 * Math.cos(theta1),\n\
+			y : -.2,\n\
+			z : (.25 * Math.sin(theta1))\n\
+		};\n\
+		var p2 = {\n\
+			x : .25 * Math.cos(theta2),\n\
+			y : -.2,\n\
+			z :  (.25 * Math.sin(theta2))\n\
+		};\n\
+		var p3 = {\n\
+			x : .25 * Math.cos(theta3),\n\
+			y : -.2,\n\
+			z : (.25 * Math.sin(theta3))\n\
+		};\n\
+		p1.r = 255;\n\
+		p2.g = 255;\n\
+		p3.b = 255;\n\
+\n\
+		var p4 = {\n\
+			x : 0,\n\
+			y : .2,\n\
+			z : 0\n\
+		};\n\
+		\n\
+		p1.g = p1.b = p2.r = p2.b = p3.r = p3.g = p4.b = 0;\n\
+\n\
+		p4.r = p4.g = 255;\n\
+		var elements =  [p1,p2,p3,p1,p4,p2,p1,p4,p3,p4,p2,p3];\n\
+		R.drawElements(elements,RenderModes.Triangles);\n\
+\n\
+		timer.x += increment.x;\n\
+		timer.y += increment.y;\n\
+		timer.z += increment.z;\n\
+		R.update();\n\
+		}\n\
+	return draw;})()"
+}
+
+
+Scenes.simpleTriangle = {
+	title:"Simple Triangle",
+	description:"Just a simple triangle",
+	drawUpdate: "(function() {\n\
+	var draw = function(){\n\
+		R.clearColor(255,255,0);\n\
+		var theta1 = 0;\n\
+		var theta2 =  (2 * Math.PI) / 3;\n\
+		var theta3 = (4 * Math.PI) / 3;\n\
+		var p1 = {\n\
+			x : .25 * Math.cos(theta1),\n\
+			y : (.25 * Math.sin(theta1)),\n\
+			r:255,g:0,b:0};\n\
+		var p2 = {\n\
+			x : .25 * Math.cos(theta2),\n\
+			y :  (.25 * Math.sin(theta2)),\n\
+			r:0,g:255,b:0};\n\
+		var p3 = {\n\
+			x : .25 * Math.cos(theta3),\n\
+			y : (.25 * Math.sin(theta3)),\n\
+			r:0,g:0,b:255};\n\
+		p1.z = p2.z = p3.z = -1.5;\n\
+		R.drawElements([p1,p2,p3],RenderModes.Triangles);\n\
+		R.update();\n\
+	}\n\
+	return draw;})()"
+}
+Scenes.sphereTexture = {
+	title:"Textured earth",
+	description:"The earth is round. Images are not. Force them to be so against their will. ",
+	drawUpdate: "(function() {\n\
+	R.setFragmentShader(textureFragShader);\n\
+	var earthWidth = 400;\n\
+	var earthHeight = 200;\n\
+	function textureFragShader(R,pt,uniform){\n\
+		var texCoordX = Math.floor((pt.theta/(2*Math.PI)) * earthWidth);\n\
+		var texCoordY = Math.floor(((pt.phi + Math.PI/2)/Math.PI) * earthHeight);\n\
+		var dataOffset = earthWidth*4*texCoordY + 4*texCoordX;\n\
+		pt.r = earthTexture[dataOffset];\n\
+		pt.g = earthTexture[dataOffset+1];\n\
+		pt.b = earthTexture[dataOffset+2];\n\
+	}\n\
+	earthTexture.src = 'http://www.shadedrelief.com/natural3/images/no_ice_clouds.jpg';\n\
+	function pointFromPolar(theta,phi,r){\n\
+		var y = r*Math.sin(phi);\n\
+		var x = r*Math.cos(phi)*Math.cos(theta);\n\
+		var z = r*Math.cos(phi)*Math.sin(theta);\n\
+		return {x:x,y:y,z:z,theta:theta,phi:phi};\n\
+	}\n\
+	var yRot = 0;\n\
+	var xRot = 0;\n\
+	function pointFromPolar(theta,phi,r){\n\
+		var y = r*Math.sin(phi);\n\
+		var x = r*Math.cos(phi)*Math.cos(theta);\n\
+		var z = r*Math.cos(phi)*Math.sin(theta);\n\
+		return {x:x,y:y,z:z,theta:theta,phi:phi};\n\
+	}\n\
+	function getSpherePoints(r,xDivs,yDivs){\n\
+		var bodyPts = [];\n\
+		for(var i = 1; i<yDivs-1; i++){\n\
+			var phi = -Math.PI/2 + (i*Math.PI)/yDivs;\n\
+			var nextPhi = phi + Math.PI/yDivs;\n\
+			for(var j = 0; j<xDivs; j++){\n\
+				var theta = (2*Math.PI*j)/xDivs;\n\
+				var nextTheta = theta + (2*Math.PI)/xDivs;\n\
+				bodyPts.push(pointFromPolar(theta,phi,r));\n\
+				bodyPts.push(pointFromPolar(theta,nextPhi,r));\n\
+				bodyPts.push(pointFromPolar(nextTheta,nextPhi,r));\n\
+				bodyPts.push(pointFromPolar(nextTheta,phi,r));\n\
+			}\n\
+		}\n\
+		//The body can be drawn in quads, but the caps require triangles\n\
+		var capPts = [];\n\
+		var capPhi = -Math.PI/2 + Math.PI/yDivs;\n\
+		for(var i = 0; i<xDivs;i++){\n\
+			var theta = (2*Math.PI*i)/xDivs;\n\
+			var nextTheta = theta + (2*Math.PI)/xDivs;\n\
+			capPts.push(pointFromPolar(theta,capPhi,r));\n\
+			capPts.push(pointFromPolar(nextTheta,capPhi,r));\n\
+			capPts.push(pointFromPolar(0,-Math.PI/2,r));\n\
+\n\
+			capPts.push(pointFromPolar(theta,-capPhi,r));\n\
+			capPts.push(pointFromPolar(nextTheta,-capPhi,r));\n\
+			capPts.push(pointFromPolar(0,Math.PI/2,r));\n\
+		}\n\
+		return {body:bodyPts,cap:capPts};\n\
+	}\n\
+	function drawSphere(spherePts){\n\
+		R.drawElements(spherePts.body,RenderModes.Quads);\n\
+		R.drawElements(spherePts.cap,RenderModes.Triangles);\n\
+	}\n\
+\n\
+	function fragShader(R,pt,uniform){\n\
+		pt.r = (pt.theta%1)*255;\n\
+		pt.g = (pt.phi%1)*255;\n\
+		pt.b = 0;\n\
+	}\n\
+	//R.setFragmentShader(fragShader);\n\
+	var spherePts = getSpherePoints(.3,6,6);\n\
+	var draw = function(){\n\
+		yRot -= Number(KeysDown[KeyCodes.LEFT])*.05;\n\
+		yRot += Number(KeysDown[KeyCodes.RIGHT])*.05;\n\
+		xRot -= Number(KeysDown[KeyCodes.UP])*.05;\n\
+		xRot += Number(KeysDown[KeyCodes.DOWN])*.05;\n\
+		R.clearColor(0,255,255);\n\
+		R.modelViewMat.loadIdentity();\n\
+		R.modelViewMat.rotate(yRot,0,1,0);\n\
+		R.modelViewMat.rotate(xRot,1,0,0);\n\
+		R.modelViewMat.translate(0,0,-1.5);\n\
+		drawSphere(spherePts);\n\
+		R.update();\n\
+	}\n\
+	return draw;})()"
+}
